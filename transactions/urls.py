7098a847add87dcb8a6d24ec0d2db09cf33252ae
Faezeh Abedi <faezeh.abedi@gmail.com>
2025-12-02 12:35:31 +0000

    Add client summary chart fixes

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import portfolio_view, download_vfd_report
+from . import views
 
 urlpatterns = [
     path("reports/portfolio/", portfolio_view, name="portfolio"),
     path("download-vfd/", download_vfd_report, name="download_vfd"),
+    path("clients/<int:client_id>/summary/",views.client_summary,name="client_summary" ),
 ]
+
+
